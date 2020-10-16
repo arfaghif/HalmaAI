7bfa4bddc10148cb04776e100604e0aab346176e
arfaghif <52845090+arfaghif@users.noreply.github.com>
2020-10-16 20:22:13 +0700

    nambahin

--- Pion.py
+++ Pion.py
@@ -21,8 +21,11 @@ class Pion():
         y1 = self.position[1] * cell_height + border_size / 2
         x2 = (self.position[0] + 1) * cell_width - border_size / 2
         y2 = (self.position[1] + 1) * cell_height - border_size / 2
-        # if(self.position==(0,0) or self.position==(0,1)):
-        #     return
-        # print("do")
-        self.canvas = board.canvas.create_oval(x1, y1, x2, y2,tags="pion",  fill=self.playerType.get_color(), activefill='cyan',  outline=None)
-        board.update()
+        
+        if(self.playerType==PlayerType.Player):
+            active_fill = 'cyan'
+        else:
+            active_fill = None
+
+        self.canvas = board.canvas.create_oval(x1, y1, x2, y2,tags="pion",  fill=self.playerType.get_color(), activefill=active_fill,  outline=None)
+        
