26185546f7cfc74ac43dc0d39a24d9f823d5af9c
arfaghif <52845090+arfaghif@users.noreply.github.com>
2020-10-17 21:18:03 +0700

    update enum

--- PlayerType.py
+++ PlayerType.py
@@ -2,9 +2,13 @@ from enum import Enum
 
 class PlayerType(Enum):
     Agent = 1
-    Player = -1
+    Player = 2
     def get_color(self):
         if(self == PlayerType.Agent):
             return ("#0D7476")
         else :
             return ("#A20202")
+
+
+# a = PlayerType(1)
+# print(a.value)
